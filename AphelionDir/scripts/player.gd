4a90caea57188645b0176ac64a6e64e94386e3d0
Apheliorex <darkdreams2675@gmail.com>
2026-07-19 08:31:22 -0700

    Door Scene Beginning
    Started creating door scene and added some new graphics.

---
+++ AphelionDir/scripts/player.gd
@@ -0,0 +1,53 @@
+extends CharacterBody2D
+
+class_name Player
+
+const BASESPEED = 100
+var speed: float
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
+func _physics_process(delta: float) -> void:
+	
+	var dir := Vector2(Input.get_axis("left", "right"), Input.get_axis("up","down")).normalized()
+	
+	if Input.is_action_pressed("shift"):
+		speed = BASESPEED + 50
+		sprite.speed_scale = 1.5
+	else:
+		speed = BASESPEED
+		sprite.speed_scale = 1
+		
+	if dir:
+		velocity = dir * speed
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, speed)
+	
+	sprite_manager(dir)
+	
+	move_and_slide()
+
+func sprite_manager(dir: Vector2) -> void:
+	var current_anim := sprite.animation
+	var front := current_anim.ends_with("front")
+	if dir == Vector2.ZERO:
+		if front:
+			sprite.animation = "idle front"
+		else:
+			sprite.animation = "idle back"
+	else:
+		if dir.y > 0:
+			sprite.animation = "walk front"
+		elif dir.y < 0:
+			sprite.animation = "walk back"
+		else:
+			if front:
+				sprite.animation = "walk front"
+			else:
+				sprite.animation = "walk back"
+			
+	if dir.x != 0:
+		if dir.x > 0:
+			sprite.flip_h = true
+		else:
+			sprite.flip_h = false
